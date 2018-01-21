1f353c48f2651a85e1f6d9004527caf536dd2f10
DiegBriceno03 <diego.briceno03@knights.ucf.edu>
2018-01-20 21:50:19 -0500

    More implementation changes

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
 const express = require('express');
 var bodyParser = require('body-parser');
 var fetch = require('node-fetch');
+var path = require('path');
 
 const app = express();
 // var jsonParser = bodyParser.json();
 
-// app.use(bodyParser.json());
-app.get('/', function(req, res){
-   res.send('Hello World!');
-})
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.post('/processForm', function(req, res){
+  let x = req.body.first;
+  console.log('Successfully uplodaded');
+});
+
+app.get('/*', function(req, res){
+   res.sendFile(path.join(__dirname + '/index.html'));
+});
 
 app.listen(3000);
 
-fetch('http://api.walmartlabs.com/v1/items?apiKey={INSERT_KEY_HERE}&upc=035000521019')
+fetch('http://api.walmartlabs.com/v1/items?apiKey={Key}&upc='+x)
     .then(res => res.json())
     .then(res => {
     console.log(res.items[0].name)
