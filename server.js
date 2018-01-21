3d6c22fd464cf7762245a60111cfdbb786c54dd1
DiegBriceno03 <diego.briceno03@knights.ucf.edu>
2018-01-20 22:51:52 -0500

    some Changes

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.use(bodyParser.json())
 
 app.post('/processForm', function(req, res){
   let x = req.body.first;
-  res.send('Successfully uplodaded');
-  fetch('http://api.walmartlabs.com/v1/items?apiKey=fhd34cqau75b2mgqxhuhjras&upc='+x)
+  // res.send('Successfully uplodaded');
+  fetch('http://api.walmartlabs.com/v1/items?apiKey=KEY&upc='+x)
       .then(res => res.json())
       .then(res => {
       console.log(res.items[0].name)
@@ -23,6 +23,11 @@ app.post('/processForm', function(req, res){
   });
 });
 
+app.get('/processForm', function(req, res){
+  var drink = [
+    {name: res.items[0].name)}
+  ]
+});
 app.get('/*', function(req, res){
    res.sendFile(path.join(__dirname + '/index.html'));
 });
